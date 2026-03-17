e1db9a3cb2cb2b0d4132d9f03b23af6017af524f
adlin15 <alin15@sfsu.edu>
2026-03-16 22:35:08 -0700

    Cleaned up progress, and made game more clear.

--- microProjSketches/p5SketchMP.js
+++ microProjSketches/p5SketchMP.js
@@ -1,70 +1,81 @@
 let circles = [];
+let squares = [];
 let score = 0;
 let boxX = 50;
 let boxY = 50;
 let boxW = 500;
 let boxH = 300;
 let isPaused = false;
-let startButton, pauseButton;
+let gameOver = false;
+let startButton, pauseButton, restartButton;
+let lastSquareTime = 0;
 
 function setup() {
   createCanvas(600, 400);
   spawnCircles(3);
+
   startButton = createButton('Start');
-  startButton.position(10, height - 40);
+  startButton.position(100, 10);
   startButton.mousePressed(startGame);
 
   pauseButton = createButton('Pause');
-  pauseButton.position(100, height - 40);
+  pauseButton.position(160, 10);
   pauseButton.mousePressed(pauseGame);
 
   restartButton = createButton('Restart');
-  restartButton.position(190, height - 40);
+  restartButton.position(230, 10);
   restartButton.mousePressed(restartGame);
 }
 
 function draw() {
   background(0);
 
-
   stroke(255);
   strokeWeight(3);
   noFill();
   rect(boxX, boxY, boxW, boxH);
 
-  if (!isPaused) {
+  if (!isPaused && !gameOver) {
     updateCircles();
+    updateSquares();
+    if (millis() - lastSquareTime > 5000) {
+      spawnSquare();
+      lastSquareTime = millis();
+    }
   }
 
-    drawCircles();
+  drawCircles();
+  drawSquares();
+
   fill(255);
   noStroke();
   textSize(20);
   textAlign(LEFT, TOP);
   text("Score: " + score, 10, 10);
-}
-  function updateCircles() {
-  for (let i = circles.length - 1; i >= 0; i--) {
-    let c = circles[i];
 
-    c.x += c.vx;
-    c.y += c.vy;
-
-    if (c.x - c.r < boxX || c.x + c.r > boxX + boxW) {
-      c.vx *= -1;
-    }
-    if (c.y - c.r < boxY || c.y + c.r > boxY + boxH) {
-      c.vy *= -1;
-    }
+  if (gameOver) {
+    fill(255, 0, 0);
+    textAlign(CENTER, CENTER);
+    textSize(40);
+    text("Game Over!", width / 2, height / 2);
   }
 }
-  function drawCircles() {
-    noStroke();
-    for (let c of circles) {
-      fill(c.col);
-      ellipse(c.x, c.y, c.r * 2, c.r * 2);
-    }
 
+function updateCircles() {
+  for (let c of circles) {
+    c.x += c.vx;
+    c.y += c.vy;
+    if (c.x - c.r < boxX || c.x + c.r > boxX + boxW) c.vx *= -1;
+    if (c.y - c.r < boxY || c.y + c.r > boxY + boxH) c.vy *= -1;
+  }
+}
+
+function drawCircles() {
+  noStroke();
+  for (let c of circles) {
+    fill(c.col);
+    ellipse(c.x, c.y, c.r * 2, c.r * 2);
+  }
 }
 
 function spawnCircles(s) {
@@ -79,32 +90,67 @@ function spawnCircles(s) {
   }
 }
 
+function spawnSquare() {
+  let size = random(20, 40);
+  let x = random(boxX + size, boxX + boxW - size);
+  let y = random(boxY + size, boxY + boxH - size);
+  squares.push({ x, y, size });
+}
+
+function updateSquares() {
+
+}
+
+function drawSquares() {
+  noStroke();
+  fill(255, 0, 0);
+  for (let s of squares) {
+    rect(s.x - s.size / 2, s.y - s.size / 2, s.size, s.size);
+  }
+}
+
 function mousePressed() {
-  if (isPaused) return;
+  if (isPaused || gameOver) return;
+
   for (let i = circles.length - 1; i >= 0; i--) {
     let c = circles[i];
     let d = dist(mouseX, mouseY, c.x, c.y);
     if (d < c.r) {
       circles.splice(i, 1);
       score++;
-
       spawnCircles(1);
-      break;
+      return;
+    }
+  }
+
+  for (let s of squares) {
+    if (
+      mouseX > s.x - s.size / 2 &&
+      mouseX < s.x + s.size / 2 &&
+      mouseY > s.y - s.size / 2 &&
+      mouseY < s.y + s.size / 2
+    ) {
+      gameOver = true;
+      isPaused = true;
+      return;
     }
   }
 }
 
 function startGame() {
-  isPaused = false;
+  if (!gameOver) isPaused = false;
 }
 
 function pauseGame() {
-  isPaused = true;
+  if (!gameOver) isPaused = true;
 }
 
 function restartGame() {
   score = 0;
   circles = [];
+  squares = [];
   isPaused = false;
+  gameOver = false;
+  lastSquareTime = millis();
   spawnCircles(3);
 }
